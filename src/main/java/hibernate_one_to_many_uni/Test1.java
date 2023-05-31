dc93887aed434b730371de62a0dc7d5771ced779
arturhasparian <a.hasparian@gmail.com>
2023-05-31 23:01:56 +0300

    change modify

--- src/main/java/hibernate_one_to_many_uni/Test1.java
+++ src/main/java/hibernate_one_to_many_uni/Test1.java
@@ -15,34 +15,34 @@ public class Test1 {
 
         Session session = null;
         try {
-//            session = factory.getCurrentSession();
-//
-//            Department dep = new Department("HR",500,1500);
-//            Employee emp1 = new Employee("Oleg", "Ivanov", 800);
-//            Employee emp2 = new Employee("Andrey","Sidorov",1000);
-//            dep.addEmployeeToDepartment(emp1);
-//            dep.addEmployeeToDepartment(emp2);
-//            session.beginTransaction();
-//
-//            session.save(dep);
-//
-//            session.getTransaction().commit();
-//            System.out.println("Done!");
+            session = factory.getCurrentSession();
+
+            Department dep = new Department("HR",500,1500);
+            Employee emp1 = new Employee("Oleg", "Ivanov", 800);
+            Employee emp2 = new Employee("Andrey","Sidorov",1000);
+            dep.addEmployeeToDepartment(emp1);
+            dep.addEmployeeToDepartment(emp2);
+            session.beginTransaction();
+
+            session.save(dep);
+
+            session.getTransaction().commit();
+            System.out.println("Done!");
 //            session = factory.getCurrentSession();
 //            session.beginTransaction();
 //            Department dep = session.get(Department.class,2);
 //
 //            System.out.println(dep);
 //            System.out.println(dep.getEpms());
-            session = factory.getCurrentSession();
-
-            session.beginTransaction();
-            Department dep = session.get(Department.class,3);
-
-            session.delete(dep);
-
-            session.getTransaction().commit();
-            System.out.println("Done!");
+//            session = factory.getCurrentSession();
+//
+//            session.beginTransaction();
+//            Department dep = session.get(Department.class,3);
+//
+//            session.delete(dep);
+//
+//            session.getTransaction().commit();
+//            System.out.println("Done!");
 
         }
         finally {
